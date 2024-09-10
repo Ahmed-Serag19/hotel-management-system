a663ed6063430b8ce5d49e961ff77695e5beb169
Sara-ibrahiim <sou.cute94@gmail.com>
2024-09-10 11:24:57 +0300

    Dependencies-LoginUi

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AuthLayout from "./modules/Shared/components/AuthLayout/AuthLayout";
+import Login from "./modules/Auth/components/Login/Login";
+import Register from "./modules/Auth/components/Register/Register";
+import ResetPassword from "./modules/Auth/components/ResetPassword/ResetPassword";
+import ForgetPassword from "./modules/Auth/components/ForgetPassword/ForgetPassword";
+import ChangePassword from "./modules/Auth/components/ChangePassword/ChangePassword";
+import Verify from "./modules/Auth/components/Verify/Verify"
+import NotFound from "./modules/Shared/components/NotFound/NotFound";
+import MasterLayout from "./modules/Shared/components/MasterLayout/MasterLayout";
+import Home from "./modules/Home/components/Home/Home";
+//import ProtectedRoute from "./modules/Shared/components/ProtectedRoute/ProtectedRoute";
+import {
+  createBrowserRouter,
+//  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+
+const routes = createBrowserRouter([
+  {
+    path:"",
+    element: <AuthLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Login/> },
+      { path: "login", element: <Login/> },
+      { path: "register", element: <Register/> },
+      { path: "change-password", element: <ChangePassword /> },
+      { path: "forget-password", element: <ForgetPassword /> }, 
+      { path: "reset-password", element: <ResetPassword /> },
+      { path: "verify", element: <Verify/> },
+    
+    ],
+  },
+  {
+    path: "dashboard",
+    element: (
+   //  <ProtectedRoute>
+        <MasterLayout/>
+    //  </ProtectedRoute>
+    ),
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "home", element: <Home /> },
+
+    ],
+  },
+])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+ <RouterProvider router={routes} />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
+// import "bootstrap/dist/js/bootstrap.bundle.min.js"
+// import "bootstrap/dist/css/bootstrap.min.css"
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ToastContainer autoClose={2000} />
+  
   </StrictMode>,
 )
