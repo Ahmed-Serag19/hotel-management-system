a888443d00d89daf6c3e1dc63936dbf2cd90fccc
Sara-ibrahiim <sou.cute94@gmail.com>
2024-09-12 18:52:52 +0300

    endPoint

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Register from "./modules/Auth/components/Register/Register";
 import ResetPassword from "./modules/Auth/components/ResetPassword/ResetPassword";
 import ForgetPassword from "./modules/Auth/components/ForgetPassword/ForgetPassword";
 import ChangePassword from "./modules/Auth/components/ChangePassword/ChangePassword";
-import Verify from "./modules/Auth/components/Verify/Verify"
 import NotFound from "./modules/Shared/components/NotFound/NotFound";
 import MasterLayout from "./modules/Shared/components/MasterLayout/MasterLayout";
 import Home from "./modules/Home/components/Home/Home";
@@ -31,8 +30,6 @@ const routes = createBrowserRouter([
       { path: "change-password", element: <ChangePassword /> },
       { path: "forget-password", element: <ForgetPassword /> }, 
       { path: "reset-password", element: <ResetPassword /> },
-      { path: "verify", element: <Verify/> },
-    
     ],
   },
   {
