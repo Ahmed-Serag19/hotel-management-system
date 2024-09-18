7037725c0bba18e516a7a96338c98fa6c8953643
youssef4nasser <youssef4nasser@gmail.com>
2024-09-18 14:30:17 +0300

    Added Modal component with form fields for room number, discount, and active status, and updated AdsList component to include a button to open the modal.

--- src/modules/Ads/Components/AdsList/AdsList.tsx
+++ src/modules/Ads/Components/AdsList/AdsList.tsx
@@ -1,5 +1,7 @@
 import {
   Box,
+  Divider,
+  MenuItem,
   Paper,
   Stack,
   styled,
@@ -9,12 +11,14 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
   Typography,
 } from "@mui/material";
 import Button from "@mui/material/Button";
 import axios from "axios";
 import { Ads_URls } from "../../../../constants/End_Points";
 import { useEffect, useState } from "react";
+import Modal from '@mui/material/Modal';
 
 interface AdsTypes {
   room: {
@@ -36,8 +40,42 @@ const StyledTableRow = styled(TableRow)(() => ({
   },
 }));
 
+const currencies = [
+  {
+    value: 'USD',
+    label: '$',
+  },
+  {
+    value: 'EUR',
+    label: '€',
+  },
+  {
+    value: 'BTC',
+    label: '฿',
+  },
+  {
+    value: 'JPY',
+    label: '¥',
+  },
+];
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 500,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 function AdsList() {
   const [ads, setAds] = useState<AdsTypes[]>([]);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const gitAdsList = async () => {
     try {
@@ -85,6 +123,7 @@ function AdsList() {
         </Box>
         <Box>
           <Button
+            onClick={handleOpen}
             sx={{ backgroundColor: "primary.main" }}
             variant="contained"
             size="large"
@@ -139,6 +178,62 @@ function AdsList() {
           </Table>
         </TableContainer> : <h1>No Data</h1> } {/* TO DO: add component NoData  */}
       </Stack>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box 
+          component="form"
+          sx={style}
+        >
+          <Typography variant="h4" fontWeight={"bold"}>Ads</Typography>
+        {/* text field for room number */}
+        <TextField
+          id="outlined-select"
+          select
+          fullWidth
+          label="Room Number"
+          defaultValue="EUR"
+          sx={{marginTop: 3}}
+          variant="filled"
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        {/* text field for Discount */}
+        <TextField
+          fullWidth
+          id="filled-basic"
+          label="Discount"
+          variant="filled"
+          sx={{marginTop: 3}}
+        />
+        {/* text field for isActive */}
+        <TextField
+          id="outlined-select-active"
+          select
+          fullWidth
+          label="Active"
+          defaultValue={'true'}
+          sx={{ marginTop: 3 }}
+          variant="filled"
+          onChange={(e) => {
+            const value = e.target.value === 'true' ? true : false;
+            console.log(value);
+          }}
+        >
+          <MenuItem value={'true'}>Yes</MenuItem>
+          <MenuItem value={'false'}>No</MenuItem>
+        </TextField>
+        <Divider sx={{marginY: 2}} />
+        <Button size="large" variant="contained">Save</Button>
+        </Box>
+      </Modal>
     </Box>
   );
 }
